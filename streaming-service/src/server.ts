2571c572359762d4c4d027da965ecc1d20d9e253
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2023-02-27 19:23:12 +1100

    Part 2 - implemented check for battery temperature exceeding safe operating range more than 3 times in 5 seconds - output to incidents.log

--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -1,10 +1,20 @@
 import net from 'net';
 import { WebSocket, WebSocketServer } from 'ws';
+import fs from 'fs';
 
 const TCP_PORT = parseInt(process.env.TCP_PORT || '12000', 10);
 
 const tcpServer = net.createServer();
 const websocketServer = new WebSocketServer({ port: 8080 });
+const batteryOperatingRange = [20, 80];
+
+const alertsQueue: Array<number> = [];
+const myConsole = new console.Console(fs.createWriteStream('./incidents.log'));
+
+function checkOperatingRange(temperature: number): boolean {
+  return temperature > batteryOperatingRange[0] && 
+    temperature < batteryOperatingRange [1];
+}
 
 tcpServer.on('connection', (socket) => {
     console.log('TCP client connected');
@@ -12,12 +22,30 @@ tcpServer.on('connection', (socket) => {
     socket.on('data', (msg) => {
         console.log(msg.toString());
 
+
         // HINT: what happens if the JSON in the received message is formatted incorrectly?
         // HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
 
         // See if message being passed can be parsed as a JSON, else skip it
         try {
+          console.log
           let currJSON = JSON.parse(msg.toString());
+
+          let timestamp = currJSON["timestamp"]
+
+          console.log(alertsQueue);
+
+          while (alertsQueue.length > 0 && alertsQueue[0] < timestamp - 5000) {
+            alertsQueue.shift();
+          }
+
+          if (!checkOperatingRange(currJSON["battery_temperature"])) {
+            alertsQueue.push(timestamp)
+          }
+
+          if (alertsQueue.length > 3) {
+            myConsole.log("alert @", timestamp);
+          }
 
           websocketServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
